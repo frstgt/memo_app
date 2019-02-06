c82868ee9c382c1a357ecb6036d3c102d2725263
U Frstgt <u.frstgt@gmail.com>
2019-02-06 02:03:18 +0000

    basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,10 @@ class User < ApplicationRecord
                        format: { with: VALID_PASSWORD_REGEX },
                        allow_nil: true
 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -7,7 +6,10 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
 
   get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
-
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 end
