2beefcc7375f5fa2104824be71c6422837961e54
U Frstgt <u.frstgt@gmail.com>
2019-05-25 05:32:17 +0000

    change app name +

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
+  def app_name
+    "Memolet"
+  end
+
   def full_title(page_title = '')
-    base_title = "Memo App"
+    base_title = app_name
     if page_title.empty?
       base_title
     else
